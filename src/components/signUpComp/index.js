55666f4482c404e8aef16d29027a86eac1acb08a
priyanshu0412 <pdevops04@gmail.com>
2025-01-18 23:35:15 +0530

    setup the navbar initially

---
+++ src/components/signUpComp/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// ----------------------------------
+
+const SignUp = () => {
+    return (
+        <>
+            This is Signup Component
+        </>
+    )
+}
+
+export default SignUp
