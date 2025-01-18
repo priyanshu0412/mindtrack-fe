af08638a132ec1f313ace3b02356b08d17d63eac
priyanshu0412 <pdevops04@gmail.com>
2025-01-18 23:02:44 +0530

    first commit

---
+++ src/components/navbar/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// ------------------------------------
+
+const Navbar = () => {
+    return (
+        <>
+            This is Navbar
+        </>
+    )
+}
+
+export default Navbar
