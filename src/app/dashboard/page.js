6ba5493c5afd9d4788914604966efd098db9574c
priyanshu0412 <pdevops04@gmail.com>
2025-01-25 23:46:28 +0530

    fixing auth flow

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+
+// -------------------------------
+
+const DashBoardPage = () => {
+    return (
+        <>
+            this is My dashboard Page
+        </>
+    )
+}
+
+export default DashBoardPage
